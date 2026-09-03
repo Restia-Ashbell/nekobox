0059b5d0c9451c7a553c3c21414eabf539354ac1
Restia-Ashbell <107416976+Restia-Ashbell@users.noreply.github.com>
2026-09-03 17:13:18 +0800

    refactor: reorganize includes and improve MainWindow functionality

--- src/system/ExternalProcess.hpp
+++ src/system/ExternalProcess.hpp
@@ -2,6 +2,10 @@
 
 #include <QProcess>
 
+namespace NekoGui_fmt {
+    struct ExternalBuildResult;
+}
+
 namespace NekoGui_sys {
     class ExternalProcess : public QProcess {
     public:
@@ -13,4 +17,9 @@ namespace NekoGui_sys {
     protected:
         bool killed = false;
     };
+
+    // Convert build results to external core processes.
+    // Note: run and start in the same thread.
+    std::list<std::shared_ptr<ExternalProcess>> CreateExtCFromExtR(
+        const std::list<std::shared_ptr<NekoGui_fmt::ExternalBuildResult>> &extRs);
 } // namespace NekoGui_sys
